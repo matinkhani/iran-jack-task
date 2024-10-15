13b9ecb297c4c40db1723672ee47dfe5242fbf27
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-10-15 09:35:31 +0330

    feat: create user details component with img and name

---
+++ src/components/layout/header/user-details.tsx
@@ -0,0 +1,12 @@
+import { STRINGS } from "../../../constants/strings";
+
+const UserDetails = () => {
+  return (
+    <div className="order-1 md:order-1 flex items-center gap-2 px-4 flex-shrink-0">
+      <img src="/images/avatar.png" className="rounded-full h-8 w-8" />
+      <h1 className="text-[#242424] text-2xl font-bold">{STRINGS.userName}</h1>
+    </div>
+  );
+};
+
+export default UserDetails;
