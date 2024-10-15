255b84bf7d4183ac2f75511b6bbd88993b60dba3
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-10-15 09:38:40 +0330

    feat: create menu bar component

---
+++ src/components/layout/menu-bar/menu-bar.tsx
@@ -0,0 +1,9 @@
+const MenuBar = () => {
+  return (
+    <div className="fixed md:relative bottom-0 w-full md:w-20 md:h-screen z-50 bg-gray-100">
+      MenuBar
+    </div>
+  );
+};
+
+export default MenuBar;
