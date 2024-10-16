91275762b496f2fdd2c607455b4228306112e938
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-10-16 19:58:22 +0330

    edit chart card styles

--- src/components/charts/chart-card.tsx
+++ src/components/charts/chart-card.tsx
@@ -4,7 +4,7 @@ import { ChartCardProps } from "./chart.type";
 
 const ChartCard: FC<ChartCardProps> = ({ title, description, children }) => {
   return (
-    <div className="p-4 rounded-lg bg-[#FFFFFF] shadow-md flex flex-col md:m-3 mb-3 md:mb-0">
+    <div className="p-4 rounded-lg bg-[#FFFFFF] shadow-lg flex flex-col md:m-3 mb-3 md:mb-0 relative">
       <div className="flex justify-between items-center">
         <div className="flex flex-col gap-">
           <p className="text-[#242424] text-sm font-bold">{title}</p>
@@ -14,10 +14,10 @@ const ChartCard: FC<ChartCardProps> = ({ title, description, children }) => {
         </div>
         <IconMore />
       </div>
-      <div className="w-full mt-3 md:mt-5 flex items-center justify-center p-4">
+      <div className="w-full mt-7 md:mt-5 flex flex-col items-center justify-center p-4">
         {children}
       </div>
-      <p className="text-[#5B5FC7] text-sm font-semibold text-right md:text-left mt-6">
+      <p className="text-[#5B5FC7] text-sm font-semibold absolute right-4 md:left-4 bottom-4">
         View details
       </p>
     </div>
