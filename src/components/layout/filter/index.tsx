30a0bbd5905e366f19ab45d4bb6f5f86f0d2f8e6
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-10-19 13:49:39 +0330

    add components to filter component

--- src/components/layout/filter/index.tsx
+++ src/components/layout/filter/index.tsx
@@ -1,7 +1,30 @@
+import Button from "../../button";
+import Divider from "../../divider";
+import IconMenu from "../../icons/icon-menu";
+import IconSearch from "../../icons/icon-search";
+import CheckBox from "../../input/check-box";
+import TextInput from "../../input/text-input";
+
 const Filter = () => {
   return (
-    <div className="hidden md:block h-12 border-b border-b-gray-200">
-      Filter
+    <div className="hidden md:flex md:items-center md:justify-between md:flex-wrap md:gap-2 px-5 py-2 border-b border-b-gray-200">
+      <div className="flex items-center">
+        <IconMenu />
+        <CheckBox lable="Text" variant="primary" />
+        <CheckBox lable="Text" variant="secondary" />
+        <CheckBox lable="Text" variant="secondary" />
+        <Divider />
+        <CheckBox lable="Text" variant="secondary" />
+        <CheckBox lable="Text" variant="secondary" />
+        <CheckBox lable="Text" variant="secondary" />
+        <Divider />
+        <CheckBox lable="Text" variant="secondary" />
+        <CheckBox lable="Text" variant="secondary" />
+      </div>
+      <div className="flex items-center justify-center gap-5">
+        <Button variant="ghost">Filter</Button>
+        <TextInput name="Find" placeholder="Find" startIcon={<IconSearch />} />
+      </div>
     </div>
   );
 };
